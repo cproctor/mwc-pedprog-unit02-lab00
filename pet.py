e1aa09fcc1c430b63142c8c031f50ddeef075207
Chris Proctor <github.com@accounts.chrisproctor.net>
2022-04-28 12:29:41 -0400

    Intending comments

--- pet.py
+++ pet.py
@@ -1,9 +1,9 @@
 class Pet:
     def __init__(self):
         '''This initializes the pet with its attributes.'''
-        self.name = None #The pet's name
-        self.tired = False #Tells us if the pet is tired
-        self.bored = True #Tells us if the pet is bored
+        self.name = None            # New pets have no name
+        self.tired = False          # New pets are not tired
+        self.bored = True           # New pets are bored
 
     def set_name(self, name):
         '''This method sets the name attribute'''
@@ -11,16 +11,16 @@ class Pet:
 
     def introduce(self):
         '''This method introduces the pet with its name.'''
-        print("Hi, I'm", self.name)
+        print(f"Hi, I'm {self.name}")
 
     def play(self):
         '''If the pet is bored, the pet will play. It will then become tired.
         If the pet is not bored, the pet will not play.
         '''
-        if self.bored == True: #The pet will only play if it's bored
+        if self.bored == True:      # The pet will only play if it's bored
             print("Let's play!!!")
-            self.bored = False #Now the pet isn't bored anymore
-            self.tired = True #But it's tired
+            self.bored = False      # Now the pet isn't bored anymore
+            self.tired = True       # But it's tired
         else:
             print("I don't want to play right now")
 
@@ -28,9 +28,9 @@ class Pet:
         '''If the pet is tired, the pet will sleep. It will then become bored.
         If the pet is not tired, the pet will not rest.
         '''
-        if self.tired == True: #The pet will only nap if it's tired
+        if self.tired == True:      # The pet will only nap if it's tired
             print("Zzzzzzzzzzzz.........")
-            self.tired = False #Now the pet isn't tired anymore
-            self.bored = True #But it's bored
+            self.tired = False      # Now the pet isn't tired anymore
+            self.bored = True       # But it's bored
         else:
             print("I'm not tired!!!!!")
